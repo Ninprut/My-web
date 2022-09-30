a0d060fdd42fabcb74500aed951e0fdf5ade14ab
Yuriy-Bohdan <112756530+Yuriy-Bohdan@users.noreply.github.com>
2022-09-30 09:13:36 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,155 @@
+var a=0;
+if(a==0){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=0;
+if(a>0){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=-3;
+if(a<0){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=0;
+if(a>=0){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=-3;
+if(a<=0){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=0;
+if(a!=0){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a="tst";
+if(a=="test"){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=3;
+if(a=="1"&& a==1){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var test=false;
+if(test==true){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var test=false;
+if(test==true){
+    console.log("true")
+}
+if(test==false) {
+    console.log("false")
+}
+
+
+var test=false;
+if(test!==true){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var test=false;
+if(test!==true){
+    console.log("true")
+}
+if(test!==false) {
+    console.log("false")
+}
+
+var a=2;
+if(a>0 && a<5){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var a=10;
+if(a==0 || a==2){
+    var b=a+7;
+    console.log(b)
+}
+else {
+    var c=a/10;
+    console.log(c)
+}
+
+var a=3;
+var b=5;
+if(a<=1 &&b>=3){
+    var c=a+b;
+    console.log(c)
+}
+else {
+    var v=a-b;
+    console.log(v)
+}
+
+var a=1;
+var b=5;
+if(a>2 && a<11 ||b>=6 && b<14){
+    
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+var num='2';
+switch (num){
+case '1':
+  var result='winter' 
+    console.log(result)
+    break;
+    case '2':
+    var result='spring' 
+    console.log(result)
+    break;
+    case '3':
+    var result='summer' 
+    console.log(result)
+    break;
+    case '4':
+    var result='autumn' 
+    console.log(result)
+    break;
+}
